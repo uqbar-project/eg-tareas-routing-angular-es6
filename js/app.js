9a0a044f49ddb805cf741039019ad73c8515053c
fdodino <fernando.dodino@gmail.com>
2017-05-18 17:14:11 -0300

    Modularization of controllers

--- js/app.js
+++ js/app.js
@@ -3,43 +3,6 @@
 /* Controllers */
 var tareasListApp = angular.module('tareasListApp', ['ui.router']);
 
-class AgregarTareaController {
-
-  constructor(TareasService) {
-    this.tareasService = TareasService;
-    this.descripcionTarea = '';
-    this.tareas = TareasService.tareas;
-  }
-  
-  agregarTarea() {
-    var tarea = this.tareasService.crearTarea(this.descripcionTarea);
-    this.tareasService.agregarTarea(tarea);
-    this.descripcionTarea = '';
-  }
-
-}
-
-class EditarTareaController {
-
-  constructor($stateParams, $state, TareasService) {
-    this.tarea = TareasService.getTareaById($stateParams.id)
-    this.state = $state
-
-    if (!this.tarea) {
-      $state.go("agregarTarea");
-      return;
-    } 
-
-    this.descripcionTarea = this.tarea.descripcion;   
-  }
-
-  aceptar() {
-    this.tarea.descripcion = this.descripcionTarea;
-    this.state.go("agregarTarea");
-  }
-
-}
-
 AgregarTareaController.$inject = ['TareasService']
 tareasListApp.controller('AgregarTareaController', AgregarTareaController)
 
--- js/tareas_service.js
+++ js/tareas_service.js
@@ -1,26 +1,26 @@
 class TareasService {
 
   constructor() {
-    this.tareasIds = 0;
-    this.tareas = [];  
+    this.tareasIds = 0
+    this.tareas = []  
   }
   
   crearTarea(description) {
-    var tarea = new Tarea(description);
-    tarea.id = this.tareasIds++;
-    return tarea;
+    var tarea = new Tarea(description)
+    tarea.id = this.tareasIds++
+    return tarea
   }
 
   agregarTarea(tarea) {
-    this.tareas.push(tarea);
+    this.tareas.push(tarea)
   }
 
   getTareaById(id) {
     return this.tareas.find((tarea) => {
-      return tarea.id == id;
+      return tarea.id == id
     })
   }
 
 }
 
-tareasListApp.service("TareasService", TareasService); 
+tareasListApp.service("TareasService", TareasService) 
